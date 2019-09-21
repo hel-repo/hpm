39998403e097e21fd4c0577453b022ce0c648eb3
Fingercomp <fingercomp@fomalhaut.me>
2019-09-21 22:28:38 +0700

    Pull specific revision of LuaMinify to avoid breakage

--- .travis/install-luaminify.sh
+++ .travis/install-luaminify.sh
@@ -1,3 +1,5 @@
-git clone https://github.com/stravant/LuaMinify ./luaminify
-mv ./luaminify/strict.lua ./luaminify/Strict.lua
-chmod +x ./luaminify/LuaMinify.sh
+git clone --depth=1 https://github.com/stravant/LuaMinify ./luaminify
+cd ./luaminify
+git reset --hard cce8f5b51bdffd1e0439956ccc308d0e5d164f90
+chmod +x ./LuaMinify.sh
+cd ..
